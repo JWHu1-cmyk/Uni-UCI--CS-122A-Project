a63267aa3e75a4aed10d33a86499e5f7044cea9d
andrewjoe2 <andrewwbarttling@gmail.com>
2024-03-10 19:00:35 -0700

    finished import added 1-6

--- project.py
+++ project.py
@@ -83,8 +83,6 @@ def add_project(ProjectID,Name,Description,CourseID,connection):
     connection.commit()
     cursor.close()
 
-
-
 def add_machine(MachineID ,Hostname, IPAddress ,OperationalStatus,Location,connection):
     machine_query = "INSERT INTO Machines (MachineID, Hostname, IPAddress, OperationalStatus, Location)VALUES ( '{}','{}','{}','{}','{}')".format(MachineID ,Hostname, IPAddress ,OperationalStatus,Location)
     cursor = connection.cursor()
@@ -114,6 +112,39 @@ def add_mange(UCINetID,MachineID,connection):
     # Committing the changes
     connection.commit()
     cursor.close()
+
+def get_table_size(table_name,connection):
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT count(*) FROM {}".format(table_name))
+
+    size = cursor.fetchone()[0]
+
+    cursor.close()
+
+    return size
+
+def deleteStudent(UCINetID,connection):
+    delete = "DELETE FROM Students WHERE UCINetID = '{}'".format(UCINetID)
+    cursor = connection.cursor()
+
+    cursor.execute(delete)
+
+    # Committing the changes
+    connection.commit()
+    cursor.close()
+
+def deleteUser(UCINetID,connection):
+    delete = "DELETE FROM Users WHERE UCINetID = '{}'".format(UCINetID)
+    cursor = connection.cursor()
+
+    cursor.execute(delete)
+
+    # Committing the changes
+    connection.commit()
+    cursor.close()
+
 
 
 def drop_table(connection):
@@ -172,7 +203,7 @@ def create_tables(connection):
     connect.execute(create_table_query)
     connection.commit()
 
-    create_table_query = "CREATE TABLE UserEmail (UCINetID VARCHAR(255) NOT NULL, Email VARCHAR(255),  PRIMARY KEY (UCINetID, Email), FOREIGN KEY (UCINetID) REFERENCES Users (UCINetID) );"
+    create_table_query = "CREATE TABLE UserEmail (UCINetID VARCHAR(255) NOT NULL, Email VARCHAR(255),  PRIMARY KEY (UCINetID, Email), FOREIGN KEY (UCINetID) REFERENCES Users (UCINetID) ON DELETE CASCADE );"
     connect = connection.cursor()
     connect.execute(create_table_query)
     connection.commit()
@@ -192,7 +223,7 @@ def create_tables(connection):
     connect.execute(create_table_query)
     connection.commit()
     
-    create_table_query = "CREATE TABLE Projects (ProjectID INT PRIMARY KEY NOT NULL,Name VARCHAR(100),Description TEXT,CourseID INT NOT NULL,FOREIGN KEY (CourseID) REFERENCES Courses(CourseID));"
+    create_table_query = "CREATE TABLE Projects (ProjectID INT PRIMARY KEY NOT NULL,Name VARCHAR(100),Description TEXT,CourseID INT NOT NULL,FOREIGN KEY (CourseID) REFERENCES Courses(CourseID) ON DELETE CASCADE);"
     connect = connection.cursor()
     connect.execute(create_table_query)
     connection.commit()
@@ -204,18 +235,17 @@ def create_tables(connection):
     connection.commit()
     #add managae and use rel tables
 
-    create_table_query =" CREATE TABLE StudentUseMachinesInProject (ProjectID INT,StudentUCINetID VARCHAR(20),MachineID INT,StartDate DATE,EndDate DATE, PRIMARY KEY (ProjectID, StudentUCINetID, MachineID),FOREIGN KEY (ProjectID) REFERENCES Projects(ProjectID),FOREIGN KEY (StudentUCINetID) REFERENCES Students(UCINetID),FOREIGN KEY (MachineID) REFERENCES Machines(MachineID));"
+    create_table_query =" CREATE TABLE StudentUseMachinesInProject (ProjectID INT,StudentUCINetID VARCHAR(20),MachineID INT,StartDate DATE,EndDate DATE, PRIMARY KEY (ProjectID, StudentUCINetID, MachineID),FOREIGN KEY (ProjectID) REFERENCES Projects(ProjectID),FOREIGN KEY (StudentUCINetID) REFERENCES Students(UCINetID),FOREIGN KEY (MachineID) REFERENCES Machines(MachineID) ON DELETE CASCADE);"
 
     
     connect = connection.cursor()
     connect.execute(create_table_query)
     connection.commit()
 
-    create_table_query = "CREATE TABLE AdministratorManageMachines (AdministratorUCINetID VARCHAR(20),MachineID INT,PRIMARY KEY (AdministratorUCINetID, MachineID),FOREIGN KEY (AdministratorUCINetID) REFERENCES Administrators(UCINetID),FOREIGN KEY (MachineID) REFERENCES Machines(MachineID));"
+    create_table_query = "CREATE TABLE AdministratorManageMachines (AdministratorUCINetID VARCHAR(20),MachineID INT,PRIMARY KEY (AdministratorUCINetID, MachineID),FOREIGN KEY (AdministratorUCINetID) REFERENCES Administrators(UCINetID),FOREIGN KEY (MachineID) REFERENCES Machines(MachineID) ON DELETE CASCADE);"
     connect = connection.cursor()
     connect.execute(create_table_query)
     connection.commit()
-
 
 def import_data(folderName:str, connection):
     try:
@@ -308,31 +338,47 @@ def import_data(folderName:str, connection):
             # Process each row
             add_mange(row[0],row[1],connection)
 
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
 
     args = sys.argv
 
     connection = connect_to_database()
 
-
-    # insert_query = create_student_query(UCINetID, First, Middle, Last)
-    # print(insert_query)
-
- 
-    # cursor = connection.cursor()
-    # cursor.execute(insert_query)
-    # connection.commit()
-
     print(args)
     if args[1] == "import":
         import_data(folderName=args[2],connection=connection)
-        
+        print(str(get_table_size("Users",connection))+","+str(get_table_size("Machines",connection))+","+str(get_table_size("Courses",connection)))
 
+    elif args[1]  == "insertStudent":
+        try:
+            add_user(args[2],args[4],args[5],args[6],connection)
+            add_student(args[2],connection)
+            add_email(args[2],args[3],connection)
+            print( True)
+        except:
+            print(False)     
+    elif args[1] == "addEmail":
+        try:        
+            add_email(args[2],args[3],connection)
+            print(True)
+        except:
+            print(False)
+    elif args[1] ==  "deleteStudent":
+        try:
+            deleteUser(args[2],connection)
+            deleteStudent(args[2],connection)
+            print(True)
+        except:
+            print(False)
+    elif args[1] =="insertMachine":
+        try:
+            add_machine(args[2],args[3],args[4],args[5],args[6],connection)
+            print(True)
+        except:
+            print(False)
+    elif args[1] == "insertUse":
+        try:
+            add_use(args[2],args[3],args[4],args[5],args[6],connection)
+            print(True)
+        except:
+            print(False)
